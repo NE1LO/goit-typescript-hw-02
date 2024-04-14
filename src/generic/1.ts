5ed106ce2c2e80c367cb40a1f6606168416e64d6
Anton Smeiko <asmeiko13@gmail.com>
2024-04-14 15:30:53 +0200

    update promiseT

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,11 +3,12 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise<T extends (string | number)[]>(): T {
+function getPromise<T extends (string | number)[]>(): Promise<T> {
   return new Promise((resolve) => {
     resolve(["Text", 50] as T);
   });
 }
+
 getPromise().then((data) => {
   console.log(data);
 });
